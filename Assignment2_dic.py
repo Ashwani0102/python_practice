68cfd70cf921d528890bcca056861c85fc4bf5a6
Ashwani0102 <163709752+Ashwani0102@users.noreply.github.com>
2025-01-21 12:50:04 +0530

    Update and rename assignement2_dic.py to Assignment2_dic.py

--- Assignment2_dic.py
+++ Assignment2_dic.py
@@ -92,3 +92,69 @@ for i in list3:
         set3.add(i)
 print(set3)  
 # Output = {2, 3}
+
+# Count the frequency of element in a list
+li = [1,2,2,3,3,3,4]
+d = {}
+for i in li:
+    if i in d:
+        d[i]=d[i]+1
+    else:
+        d[i]=1    
+print(d) 
+# # Output : {1: 1, 2: 2, 3: 3, 4: 1}
+
+# Merge 2 dictionary
+d1={"a":1,"b":2}
+d2={"b":1,"c":1} 
+d={}
+for key,value in d1.items():
+    # d[key]=d1.get(key,value)
+    d[key]=value
+for key,value in d2.items():
+    if key not in d:
+        d[key]=value
+    else:
+        d[key]=d[key] +1       
+print(d)
+# # Output : {'a': 1, 'b': 3, 'c': 1}
+
+# Group words by length in dictionary where keys are the lengths of words and value are lists of words of that length
+words =['cat','dog','elephant','bat']
+d = {}
+for i in words:
+    length=len(i)
+    if length in d:
+        d[length].append(i)
+    else:
+        d[length]=[i]    
+print(d) 
+# # Output : {3: ['cat', 'dog', 'bat'], 8: ['elephant']}  
+
+# Most Frequent Character in a String
+word="ashwani"
+d={}
+for i in word:
+    if i in d:
+        d[i]=d[i]+1
+    else:
+        d[i]=1
+max_count=0
+max_frequent_charater =""
+for key,value in d.items():
+    if value>max_count:
+        max_count=value
+        max_frequent_charater=key
+print("Max frequency amoung the character is :",max_frequent_charater,"having count as :",max_count)
+# # Output : Max frequency amoung the character is : a having count as : 2
+
+# Invert a dictionary
+d1={'a':1,'b':2,'c':1}
+d ={}
+for key,value in d1.items():
+    if value in d:
+       d[value].append(key) 
+    else:
+        d[value]=[key]
+print(d) 
+# # Output : {1: ['a', 'c'], 2: ['b']} 
